a5d4075c077ebd867fc8d536542b34a0d6da6dbf
MadsGET <69505440+MadsGET@users.noreply.github.com>
2020-09-11 15:11:07 +0200

    Update 1 11.09.20
    -Added animation smoke effect

--- data.js
+++ data.js
@@ -11,28 +11,13 @@ var content1smallText =
 // Static HTML code
 var headerHTML = `
 <div class="header">
-	<div class="headerLayerA">
-		<div class="decorationL" style="background-position: center right;"></div>
-		<div class="buttonL"><p class="navText lText" style="margin-right: 0;">CONTACT</p></div>
-		<div class="buttonM"><p class="navText hText">ABOUT</p></div>
-		<div class="buttonR"><p class="navText lText" style="margin-left: 0;">PORTFOLIO</p></div>
-		<div class="decorationR" style=" background-position: center left;"></div>
-	</div>
-	<div class="headerLayerB"></div>
 </div> `
 
 var content1HTML = `	
 <div class="content">
-	<div class="contentLayerA">
-			<div class="contentHead"><h1>< Hello World ></h1></div>
-			<div class="contentBoxL"></div>
-			<div class="contentBoxS">${content1smallText}</div>
-			<div class="contentPortrait"></div>
-	</div>
-	<div class="contentLayerB"></div>
 </div>`;
 
 var footerHTML = `			
 <div class="footer">
-	<p class="footerText">Copyright 2020</p>
+
 </div>`;
