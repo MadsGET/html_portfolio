19eb682176231d3b6d0f72ea29b2f264b4b52cb6
MadsGET <69505440+MadsGET@users.noreply.github.com>
2020-09-11 23:17:50 +0200

    Major Update
    - New button design
    - New header scaling capabilities
    - Smoke background effect bug fix

--- data.js
+++ data.js
@@ -1,16 +1,18 @@
-
-// Static text.
-var content1smallText =
-// Det ligger en bug her?
-` 
-<p style="Text-Align: left; float:left; margin-left: 2.5%">[-] NAME:  <br> [-] DOB:  <br> [-] LOCATION:</p>
-<p style="Text-Align: right; float:right; margin-right: 2.5%">Mads  <br> 1994  <br> Norway</p>
-<small> Experience: C#, Javascript, HTML, CSS, Photoshop, Unity3D, Blender.</small>
-`;
+var urlButtonL = ['buttonLarge_Contact', 'buttonLarge_About', 'buttonLarge_Portfolio'];
+var urlButtonS = ['buttonSmall_Contact', 'buttonSmall_About', 'buttonSmall_Portfolio'];
 
 // Static HTML code
 var headerHTML = `
 <div class="header">
+	<div class="headerBackground"></div>
+	<div class="headerContainer">
+		<div class="headerDecoration" style="background-position: center right;"></div>
+		<div class="headerButton" style="background-image: url(images/Buttons/${urlButtonS[0]}.png)"></div>
+		<div class="headerButton" style="background-image: url(images/Buttons/${urlButtonL[1]}.png)"></div>
+		<div class="headerButton" style="background-image: url(images/Buttons/${urlButtonS[1]}.png)"></div>
+		<div class="headerDecoration" style="background-position: center left;"></div>
+	</div>
+	
 </div> `
 
 var content1HTML = `	
