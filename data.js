6bef7657a5249a555404dd9260270c68107e0ac0
MadsGET <69505440+MadsGET@users.noreply.github.com>
2020-09-16 20:29:54 +0200

    Additional Mobile Scaling
    - Added the ability to remove decoration UI in the header when screen size is very small.

--- data.js
+++ data.js
@@ -1,6 +1,5 @@
-const urlButtonL = ['buttonLarge_Contact', 'buttonLarge_About', 'buttonLarge_Portfolio'];
-const urlButtonS = ['buttonSmall_Contact', 'buttonSmall_About', 'buttonSmall_Portfolio'];
-const contentAnimState = ['fadeIn', 'fadeOut'];
+const buttonSrc = ["url('images/button_Contact.svg')", "url('images/button_About.svg')", "url('images/button_Portfolio.svg')"]
+const pageTitle = ['Contact', 'About', 'Portfolio'];
 
 // Static text fields
 const largeInfo = `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer eleifend quis lorem molestie sagittis. Donec tempus, purus sit amet blandit scelerisque, tellus quam dignissim orci, quis tempus mi tortor sed urna. Vestibulum eget dolor quis magna efficitur tempus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Aliquam erat volutpat. Nulla facilisi. Morbi et orci eget eros congue fermentum. Aenean interdum luctus mi, ut maximus erat ultrices vitae. Aenean blandit tempor ornare. Duis nisi dolor, condimentum quis dolor vitae, volutpat bibendum odio. Quisque congue nulla odio. In ultricies, eros eu condimentum dignissim, nisl dui ultrices ligula, vitae blandit risus dui dictum dolor. Class aptent taciti sociosquo.`
@@ -8,7 +7,30 @@ const largeInfo = `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Inte
 // Static HTML code
 function getHeader(buttonIndex)
 {
-	return  `<div class="header"></div>` 
+	return `
+	<div class="header">
+		<div class="headerContainer">				
+			<div class="headerDecoration" style="grid-area:'decorationL'">
+				<div class="decorationImage"><div class="decorationMirror" style="opacity:0.5"></div></div>
+			</div>
+			<div class="headerContainerL">
+				<div class="headerButton" onClick="onPageSelection(0, currentButtonIndex[0])" style="background-image: ${buttonSrc[buttonIndex[0]]};"></div>
+				<div class="headerButtonImage"></div>
+			</div>		
+			<div class="headerContainerM">
+				<div class="headerButton" onClick="onPageSelection(1, currentButtonIndex[1])" style="background-image: ${buttonSrc[buttonIndex[1]]}; background-size: 80% 70%; opacity:1.0;"></div>
+				<div class="headerButtonImage"></div>
+			</div>
+			<div class="headerContainerR">
+				<div class="headerButton" onClick="onPageSelection(2, currentButtonIndex[2])" style="background-image: ${buttonSrc[buttonIndex[2]]};"></div>
+				<div class="headerButtonImage"></div>
+			</div>
+			<div class="headerDecoration" style="grid-area:'decorationR';">
+				<div class="decorationImage"></div>
+			</div>
+		</div>
+	</div>
+	` 
 	
 }
 
--- script.js
+++ script.js
@@ -1,37 +1,14 @@
 // Model
 var currentPageIndex = 1;
 var currentButtonIndex = [0, 1, 2];
-var currentAnimState = 0;
 
 // View
 function drawView()
 {
 	document.getElementById('page').innerHTML = `
-		<div class="header">
-			<div class="headerContainer">
-				
-				<div class="headerDecoration" style="grid-area:'decorationL'">
-					<div class="decorationImage"><div class="decorationMirror" style="opacity:0.5"></div></div>
-				</div>
-				<div class="headerContainerL">
-					<div class="headerButton" style="background-image: url('images/button_Contact.svg');"></div>
-					<div class="headerButtonImage"></div>
-				</div>		
-				<div class="headerContainerM">
-					<div class="headerButton" style="background-image: url('images/button_About.svg');	background-size: 80% 70%; opacity:1.0;"></div>
-					<div class="headerButtonImage"></div>
-				</div>
-				<div class="headerContainerR">
-					<div class="headerButton" style="background-image: url('images/button_Portfolio.svg');"></div>
-					<div class="headerButtonImage"></div>
-				</div>
-				<div class="headerDecoration" style="grid-area:'decorationR';">
-					<div class="decorationImage"></div>
-				</div>
-			</div>
-		</div>
+		${getHeader(currentButtonIndex)}
 		<div class="content">
-			<div class="contentContainer"><div class="content1HTML"></div></div>
+			<div class="contentContainer"></div>
 		</div>
 		<div class="footer">
 		</div>
@@ -51,10 +28,12 @@ function onPageSelection(buttonIndex, pageIndex)
 	currentButtonIndex[buttonIndex] = currentPageIndex;
 	currentButtonIndex[1] = pageIndex;
 	console.log(currentButtonIndex);
-	//console.log('From: ' + 	currentButtonIndex[buttonIndex] + ' To: ' + currentButtonIndex[1]);
 
 	// Set current page index to new index.
 	currentPageIndex = pageIndex;
+
+	// Set head title
+	document.title = pageTitle[currentPageIndex];
 
 	// Draw view
 	drawView();
