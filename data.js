9a2b60e375d1a90f5d4ceef6d884a23a1f6bd5d1
MadsGET <69505440+MadsGET@users.noreply.github.com>
2020-09-26 12:49:02 +0200

    Last version 4 update

--- data.js
+++ data.js
@@ -2,7 +2,15 @@ const buttonSrc = ["url('images/button_Contact.svg')", "url('images/button_About
 const pageTitle = ['Contact', 'About', 'Portfolio'];
 
 // Static text fields
-const largeInfo = `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer eleifend quis lorem molestie sagittis. Donec tempus, purus sit amet blandit scelerisque, tellus quam dignissim orci, quis tempus mi tortor sed urna. Vestibulum eget dolor quis magna efficitur tempus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Aliquam erat volutpat. Nulla facilisi. Morbi et orci eget eros congue fermentum. Aenean interdum luctus mi, ut maximus erat ultrices vitae. Aenean blandit tempor ornare. Duis nisi dolor, condimentum quis dolor vitae, volutpat bibendum odio. Quisque congue nulla odio. In ultricies, eros eu condimentum dignissim, nisl dui ultrices ligula, vitae blandit risus dui dictum dolor. Class aptent taciti sociosquo.`
+const largeInfo = `Welcome to my portfolio site, I to want first thank you for taking an into intrest in me and my website.
+Secondly I would like to note that everything in this website you see or interact with is created by yours truly. 
+I began messing around with computer hardware and software at a young age. Later as I grew so did my intrest into the field of IT.
+From building my own computer to creating video game assets such as 3D models and scripts. <br> <br>
+
+The result is that over the years I have accumulated a wide skillset that enables me to rapidly create prototypes of games, 
+applications and websites. Since I can master both the visual and programming side of things, the question I will pose to you is this;
+Why hire one person for each job, why not hire one jack of all trades instead?`
+
 const smallInfoL = `[-]Name: <br> [-]DOB: <br> [-]Location: <br> `;
 const smallInfoR = `Mads <br> 1994 <br> Norway`;
 const smallInfoB = `Experience: C#, Javascript, HTML, CSS, Photoshop, Unity3D, Blender.`;
